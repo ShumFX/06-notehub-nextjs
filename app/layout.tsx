3ce830654a7ac7a24d10c568194e32c2ebd5742d
ShumFX <alfadnepr@gmail.com>
2025-08-20 07:34:16 +0300

    work8

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import dynamic from "next/dynamic";
+import Providers from "../components/Providers/Providers";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import "./globals.css";
@@ -15,12 +15,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// Динамический импорт TanStackProvider с отключением SSR
-const TanStackProvider = dynamic(
-  () => import("../components/TanStackProvider/TanStackProvider"),
-  { ssr: false }
-);
-
 export const metadata: Metadata = {
   title: "NoteHub - Manage Your Notes",
   description: "A simple and efficient application for managing personal notes",
@@ -34,11 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <TanStackProvider>
+        <Providers>
           <Header />
           {children}
           <Footer />
-        </TanStackProvider>
+        </Providers>
       </body>
     </html>
   );
