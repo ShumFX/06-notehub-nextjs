294a5f48ec7ded44e2d712d18b4e73cf87a07c58
ShumFX <alfadnepr@gmail.com>
2025-08-24 11:52:12 +0300

    work finish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import type { Metadata } from 'next';
-import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
-import Header from '@/components/Header/Header';
-import Footer from '@/components/Footer/Footer';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -13,11 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <TanStackProvider>
-          <Header />
-          <main>{children}</main>
-          <Footer />
-        </TanStackProvider>
+        {children}
       </body>
     </html>
   );
